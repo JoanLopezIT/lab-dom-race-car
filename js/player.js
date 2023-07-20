517aff70a810337c5a8f4d3eff78e48c1667c68c
JoanLopezIT <joan.lopez.job@gmail.com>
2023-07-20 17:58:17 +0200

    done

--- js/player.js
+++ js/player.js
@@ -1,3 +1,69 @@
 class Player{
-    
+    constructor(gameScreen, left, top, width, height, imgSrc){
+        this.gameScreen = gameScreen;
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = height;
+        this.directionX = 0;
+        this.directionY = 0;
+        this.element = document.createElement("img"); // es el coche
+
+// le damos forma al coche
+        this.element.src = imgSrc;
+        this.element.style.position = "absolute";
+        this.element.style.width = `${width}px`;
+        this.element.style.height = `${height}px`;
+        this.element.style.left = `${left}px`;
+        this.element.style.top = `${top}px`;
+
+        this.gameScreen.appendChild(this.element);
+    }
+
+
+    move(){
+        this.left += this.directionX;
+        this.top += this.directionY;
+
+     // esto permite que el coche no sobrepase los limites de la pantalla
+
+        if(this.left < 0){
+            this.left = 0
+        }
+        if(this.top < 0){
+            this.top = 0
+        }
+
+        if(this.left > this.gameScreen.offsetWitdth - this.width){
+        this.left = this.gameScreen.offsetWitdth - this.width
+}
+
+   if(this.top > this.gameScreen.offsetHeight - this.height){
+   this.top = this.gameScreen.offsetHeight - this.height
+}
+this.updatePosition();
+    }
+
+    updatePosition(){
+        this.element.style.left = `${this.left}px`
+        this.element.style.top = `${this.top}px`
+    }
+    didCollide(obstacle){
+        // creamos la hitbox con getbounding
+        const playerHitbox = this.element.getBoundingClientRect();
+        const obstacleHitBox = obstacle.element.getBoundingClientRect();
+
+        if(
+            playerHitbox.left < obstacleHitBox.right &&
+            playerHitbox.right > obstacleHitBox.left &&
+            playerHitbox.top < obstacleHitBox.bottom &&
+            playerHitbox.bottom > obstacleHitBox.top 
+        ){
+            return true
+        }
+        else{
+            return false;
+        }
+    }    
+
 }
--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
+// SCRIPT
 window.onload = function () {
+  console.log('SCRIPT JS IS WORKING');
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
-
+  let game;
   startButton.addEventListener("click", function () {
+    console.log('START BUTTON IS WORKING');
     startGame();
   });
 
   function startGame() {
     console.log("start game");
+    game = new Game();
+    game.start();
   }
+  function handleKeyboardInput(key){
+    if(key === "ArrowUp"){
+      game.player.directionY = -1; // moving vertycally up = -1
+    }
+    else if(key === "ArrowDown"){
+      game.player.directionY = 1;
+    }
+    else if(key === "ArrowLeft"){
+      game.player.directionX = -1;
+    }
+    else if(key === "ArrowRight"){
+      game.player.directionX = 1;
+    }
+  }
+  window.addEventListener("keydown",(event)=>{ // keydown es pulsar cualquier tecla, keyUp es cuando dejas de pulsar
+    handleKeyboardInput(event.key)
+  })
+
 };
